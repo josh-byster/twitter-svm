d31554b3a63ef3cf9b53565973421c68cff88477
josbys1 <josbys1@nilesk12.org>
2015-11-18 20:59:52 +0000

    added randomization for test/train data

--- project.py
+++ project.py
@@ -2,10 +2,11 @@ from __future__ import absolute_import, print_function
 from parser import process_tweets
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
+from sklearn import cross_validation
 import numpy
 numpy.set_printoptions(threshold=numpy.nan)
 
-args=process_tweets("potus","who","imdb",10)
+args=process_tweets("potus","who","imdb",10) #Returns tuple value in form (data,names)
 vectorizer = CountVectorizer()
-fit_array = numpy.array(vectorizer.fit_transform(args).toarray())
-print(fit_array)
+fit_array = numpy.array(vectorizer.fit_transform(args[0]).toarray())
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(fit_array, args[1], test_size=0.33, random_state=42)

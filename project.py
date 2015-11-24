bfbf7d99e0f61a35daef063d6456bf792c9b5053
Josh Byster <jbyster2@illinois.edu>
2015-11-24 21:06:41 +0000

    dumps test/train data into pkl and npy files

--- project.py
+++ project.py
@@ -1,17 +1,21 @@
 from __future__ import absolute_import, print_function
-import parser
+import retrieve
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
 from sklearn import cross_validation
 import sklearn
 import numpy
-import pandas as pd
+from sklearn.externals import joblib
 numpy.set_printoptions(threshold=numpy.nan)
 
-args=parser.parse(["nbcnews","who","imdb"],2) #Returns tuple value in form (data,names) - number is in tuple form
+args=retrieve.parse(["nbcnews","who","imdb"],50) #Returns tuple value in form (data,names) - number is in tuple form
 vectorizer = CountVectorizer()
 fit_array = numpy.array(vectorizer.fit_transform(args[0]).toarray())
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(fit_array, args[1], test_size=0.33, random_state=42)
+joblib.dump(X_train,'Xtrain.pkl')
+joblib.dump(y_train,'Ytrain.pkl')
+joblib.dump(X_test,'Xtest.pkl')
+joblib.dump(y_test,'Ytest.pkl')
 clf = SVC(gamma=0.001,C=100)
 clf.fit(X_train,y_train)
 pred_values=clf.predict(X_test[:1000])
